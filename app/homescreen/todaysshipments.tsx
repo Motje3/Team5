f46af9700969b5006883515de6edb975e8179c18
Damian5314 <damianwillmse@gmail.com>
2025-04-20 18:20:01 +0200

    Titel on page

--- app/homescreen/todaysshipments.tsx
+++ app/homescreen/todaysshipments.tsx
@@ -1,15 +1,32 @@
+import { View, Text, Image, TouchableOpacity, StatusBar } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { useRouter } from 'expo-router';
 
 
 const Todaysshipment = () => {
     return (
         <LinearGradient
-              colors={["#3D0F6E", "#030014"]} // Extra middle color for smoothness
-              locations={[0, 0.7, 1]} // Controls smooth transition
-              start={{ x: 0.5, y: 0 }} 
-              end={{ x: 0.5, y: 0.25 }} // Stops the gradient sooner
-              style={{ flex: 1, paddingHorizontal: 24, paddingTop: 40 }} 
+            colors={["#3D0F6E", "#030014"]}
+            locations={[0, 0.7, 1]}
+            start={{ x: 0.5, y: 0 }} 
+            end={{ x: 0.5, y: 0.25 }}
+            style={{ flex: 1, paddingHorizontal: 24, paddingTop: 40 }} 
             >
+
+            <ExpoStatusBar hidden />
+            <StatusBar hidden />
+
+            {/* 🔹 Welcome Header */}
+                <View className="flex-row justify-between items-center">
+                    <View>
+                        <Text className="text-white text-2xl font-bold">Zendingen van vandaag</Text>
+                    </View>
+                </View>
+            
             
         </LinearGradient>
     );
