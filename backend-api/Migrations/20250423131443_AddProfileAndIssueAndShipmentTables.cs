da1c7acae1b3719e48f7315ca4c326b2955bac32
Wishant010 <wishantbhajan@outlook.com>
2025-04-23 17:47:24 +0200

    migration profileAnd ....

---
+++ backend-api/Migrations/20250423131443_AddProfileAndIssueAndShipmentTables.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileAndIssueAndShipmentTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Profiles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Profiles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Profiles");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Profiles");
+        }
+    }
+}
