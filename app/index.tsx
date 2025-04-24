397d8fd492b08c6546db0d556a40c55bff953b66
Motje <Mohamaadflaha2014@gmail.com>
2025-04-24 15:32:01 +0200

    fixed the problem!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/index.tsx
+++ app/index.tsx
@@ -18,7 +18,9 @@ export default function IndexRedirect() {
   }, [isLoading]);
 
   return (
-    <View>…spinner…</View>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <ActivityIndicator size="large" color="#7C3AED" />
+        </View>
   );
 }
 
