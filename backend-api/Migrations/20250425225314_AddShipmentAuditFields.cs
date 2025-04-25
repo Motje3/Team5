4114d89f1dc2bcf5905cb9615aa8f0b6a2eccd99
Motje <Mohamaadflaha2014@gmail.com>
2025-04-26 00:54:30 +0200

    new property for who and when was the shipment updated

---
+++ backend-api/Migrations/20250425225314_AddShipmentAuditFields.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShipmentAuditFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpectedDelivery",
+                table: "Shipments",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Destination",
+                table: "Shipments",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedAt",
+                table: "Shipments",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastUpdatedBy",
+                table: "Shipments",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedAt",
+                table: "Shipments");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "Shipments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpectedDelivery",
+                table: "Shipments",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Destination",
+                table: "Shipments",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
