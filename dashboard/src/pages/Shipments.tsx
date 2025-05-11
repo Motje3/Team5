926d2513d0c975457ac7b9dc3afac4d5ef325bd1
Damian5314 <damianwillmse@gmail.com>
2025-05-11 22:03:17 +0200

    Add interface

--- dashboard/src/pages/Shipments.tsx
+++ dashboard/src/pages/Shipments.tsx
@@ -1,5 +1,18 @@
+
+interface Shipment {
+  id: number;
+  status: string;
+  destination: string;
+  assignedTo: string;
+  expectedDelivery: string;
+  weight: string;
+  createdAt: string;
+  lastUpdatedBy: string;
+  lastUpdatedAt: string;
+}
+
 const Shipments = () => {
-    return <></>;
+  return <></>;
 
 };
 
