7e152f2177795bd7e36e377f2d51440a0bf8d791
herkam3 <1078244@hr.nl>
2025-05-13 14:24:29 +0200

    Stats page on dashboard. Mock stats en imports

---
+++ dashboard/src/pages/Stats.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LineChart, Line
+} from 'recharts';
+
+const mockStats = {
+  totaalKlanten: 5251,
+  zendingenVandaag: 7916,
+  maandZendingen: 59525,
+  jaarOmzet: 65152,
+};
